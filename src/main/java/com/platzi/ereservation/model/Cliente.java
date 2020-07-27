72214549e5320c9cf8d0bb9b42e4e7c36704f065
nglcode <nglcode@gmail.com>
2020-07-27 18:46:11 +0200

    Primera clase y uso de lombok

---
+++ src/main/java/com/platzi/ereservation/model/Cliente.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.platzi.ereservation.model;
+
+import lombok.Data;
+
+/**
+ * Clase que representa la tabla Cliente
+ * @author agdev
+ *
+ */
+
+@Data
+public class Cliente {
+
+	String nombreCli;
+	String apellidoCli;
+	
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
